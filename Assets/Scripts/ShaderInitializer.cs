23bc888418ab1345892a53462e95a68725285ec1
Kares <carlosegmt23@gmail.com>
2025-01-16 22:37:34 +0100

    Screen space outline

--- Assets/Scripts/ShaderInitializer.cs
+++ Assets/Scripts/ShaderInitializer.cs
@@ -5,18 +5,15 @@ public class ShaderInitializer : MonoBehaviour
     [SerializeField] private Texture _texture;
     [SerializeField] private Color _colorMult;
     [SerializeField] private bool _usesColorMult = false;
-    [SerializeField] private float _outlineScale = 1.01f;
     private Renderer _rend;
 
     void Start()
     {
         _rend = GetComponent<Renderer>();
         //Se crean copias de los materiales personalizadas para este objeto
-        _rend.materials[0] = new Material(_rend.materials[0]);
-        _rend.materials[1] = new Material(_rend.materials[1]);
+        _rend.material = new Material(_rend.material);
         //Se asignan las propiedades deseadas
-        _rend.materials[0].SetTexture("_Texture", _texture);
-        if(_usesColorMult) _rend.materials[0].SetColor("_Color", _colorMult);
-        _rend.materials[1].SetFloat("_Scale", _outlineScale);
+        _rend.material.SetTexture("_Texture", _texture);
+        if(_usesColorMult) _rend.material.SetColor("_Color", _colorMult);
     }
 }
